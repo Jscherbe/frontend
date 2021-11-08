c02d4c6a6e9d66868d0d344cdeb2d68a1d13559c
Joseph Scherben <jscherbe@gmail.com>
2021-11-07 20:09:49 -0500

    Nuxt test

---
+++ site/pages/sass.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    sass
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log(this.$content);
+    },
+    async asyncData({ $content }) {
+      const data = await $content("sassdocs").fetch();
+      console.log(data.length);
+      console.log(typeof data);
+      console.log(Array.isArray( data));
+      return { sass: data };
+    }
+  }
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,11 @@
+// Used for creating the dist versions
 const { resolve } = require("path");
 const { mixin, merge } = require("@ulu/webpack-mixin");
-const { webpackPlugin: docsPlugin } = require("./build/docs.js");
 
 module.exports = (env, argv) => {
-  const plugins = [];
-  if (env.docs) {
-    plugins.push(docsPlugin);
-  }
-  const config = merge(mixin(env, argv), {
+  return merge(mixin(env, argv), {
     entry: {
       "ulu-frontend.min" : resolve(__dirname, "index.js"),
-    },
-    plugins
-  });
-
-  // Not proxying just serving the docs folder
-  delete config.devServer.proxy;
-  const serverConfig = {
-    devServer: {
-      watchFiles: [
-        // "scss/**/*.scss",
-        // "**/*.js",
-      ],
-      static: {
-        directory: resolve(__dirname, "docs/"),
-        watch: false
-      }
     }
-  };
-  const finalConfig = merge(config, serverConfig);
-  // console.log(finalConfig);
-  return finalConfig;
+  });
 };
