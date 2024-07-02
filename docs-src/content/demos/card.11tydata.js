2e9f4d0bea3fd9683e80e3e008409c12dfea2062
Merritt Informatics <daniel.merritt@informatics-studio.com>
2024-07-02 11:23:38 -0400

    setup cards

---
+++ docs-src/content/demos/card.11tydata.js
@@ -0,0 +1,8 @@
+export default {
+  cardTypes: {
+    cardImage: '<div class="card"><div class="card__title">My first Card</div><div class="card__body">My first Card Body</div><div class="card__footer">My first Card Footer</div><div class="card__image"><img src="/assets/placeholder/image-1.jpg" /></div></div>',
+    cardIcon: '<div class="card"><div class="card__title">My first Card</div><div class="card__body">My first Card Body</div><div class="card__footer">My first Card Footer</div><div class="card__image--icon"><img src="/assets/placeholder/image-1.jpg" /></div></div>',
+    cardMedia: '<div class="card"><div class="card__title">My first Card</div><div class="card__body">My first Card Body</div><div class="card__footer">My first Card Footer</div><img class="card__image-media" src="/assets/placeholder/image-1.jpg" /></div>'
+  }
+  
+}
---
+++ docs-src/content/demos/card.md
@@ -0,0 +1,15 @@
+---
+title: Card
+layout: default
+intro: Cards are...
+
+---
+
+## Card Defaults
+
+
+{% for card in cardTypes %}
+
+{{card}}
+
+{% endfor %}
