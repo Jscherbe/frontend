7d672fcffc2b56f4f11d387dadaa5feea806ba74
Merritt Informatics <daniel.merritt@informatics-studio.com>
2024-07-02 12:15:59 -0400

    setup covered card

--- docs-src/content/demos/card.11tydata.js
+++ docs-src/content/demos/card.11tydata.js
@@ -1,7 +1,7 @@
 export default {
   cardTypes: {
-    cardImage: '<div class="card"><div class="card__title">My first Card</div><div class="card__body">My first Card Body</div><div class="card__footer">My first Card Footer</div><div class="card__image"><img src="/assets/placeholder/image-1.jpg" /></div></div>',
-    cardIcon: '<div class="card"><div class="card__title">My first Card</div><div class="card__body">My first Card Body</div><div class="card__footer">My first Card Footer</div><div class="card__image--icon"><img src="/assets/placeholder/image-1.jpg" /></div></div>',
+    cardImage: '<div class="card"><div class="card__title">Card with Image</div><div class="card__body">This is a card with an upper image using a containing div with the "card__image" class. </div><div class="card__footer">My first Card Footer</div><div class="card__image"><img src="/assets/placeholder/image-1.jpg" /></div></div>',
+    cardIcon: '<div class="card"><div class="card__title">Card with Icon</div><div class="card__body">This is a card with a lower icon or image using a containing div with the "</div><div class="card__footer">My first Card Footer</div><div class="card__image-icon"><img src="/assets/placeholder/image-1.jpg" /></div></div>',
     cardMedia: '<div class="card"><div class="card__title">My first Card</div><div class="card__body">My first Card Body</div><div class="card__footer">My first Card Footer</div><img class="card__image-media" src="/assets/placeholder/image-1.jpg" /></div>'
   }
   
--- docs-src/content/demos/card.md
+++ docs-src/content/demos/card.md
@@ -7,6 +7,16 @@ intro: Cards are...
 
 ## Card Defaults
 
+<div class="card card__covered">
+  <div class="card__content">
+    <div class="card__title">Card with Image</div>
+    <div class="card__body">This is a card with an upper image using a containing div with the "card__image" class. </div>
+    <div class="card__footer">My first Card Footer</div>
+  </div>
+  <!-- <div class="card__background"> -->
+    <img class="card__background" src="/assets/placeholder/image-1.jpg" />
+  <!-- </div> -->
+</div>
 
 {% for card in cardTypes %}
 
