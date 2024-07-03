f9951f0490de76f0850877382400956a11c723d8
Merritt Informatics <daniel.merritt@informatics-studio.com>
2024-07-03 10:00:49 -0400

    card--covered push for review

--- docs-src/content/demos/card.11tydata.js
+++ docs-src/content/demos/card.11tydata.js
@@ -28,7 +28,7 @@ export default {
             <a class="site-card__link card__title" href="#" data-proxy-click-source="">Card with Image</a>
           </div>
           <div class="site-card__body-content">
-            This is a card with an upper image using a containing div with the "card__image" class. 
+            This is a card with an upper image using a containing div with the "card__image--icon" class. This maintains the card's padding, so it will line up properly to the text.
           </div>
         </div>
         <div class="card__image card__image--icon site-card__image">
@@ -39,13 +39,23 @@ export default {
         </div>
       </article>`,
     cardCovered: `
-      <div class="card card--covered">
-        <div class="card__content">
-          <div class="card__title">Card with Image</div> 
-          <div class="card__body">This is a card with an upper image using a containing div with the "card__image" class. </div>
-          <div class="card__footer">My first Card Footer</div>
+      <span class="site-card__label">Card Label</span>
+      <span class="site-card__title">Title</span>
+      <article class="card card--covered" data-proxy-click>
+        <div class="card__body">
+          <div class="site-card__header">
+            <a class="site-card__link card__title" href="#" data-proxy-click-source="">Card with background image</a>
+          </div>
+          <div class="site-card__body-content">
+            Adding "card--covered" to the parent card to have the image cover the entire background. Text will be displayed over a semi-transparent gradient.
+          </div>
         </div>
-        <img class="card__background" src="/assets/placeholder/image-1.jpg" />
-      </div>`
+        <div class="card__image site-card__image">
+          <img src="/assets/placeholder/image-1.jpg" />
+        </div>
+        <div class="card__footer">
+          My first Card Footer
+        </div>
+      </article>`
   }
 }
--- docs-src/content/demos/card.md
+++ docs-src/content/demos/card.md
@@ -7,24 +7,6 @@ intro: Cards are...
 
 ## Card Defaults
 
-<span class="site-card__label">Card Label</span>
-<span class="site-card__title">Title</span>
-<article class="card card--covered" data-proxy-click>
-  <div class="card__body">
-    <div class="site-card__header">
-      <a class="site-card__link card__title" href="#" data-proxy-click-source="">Card with background image</a>
-    </div>
-    <div class="site-card__body-content">
-      This is a card with an upper image using a containing div with the "card__image" class. 
-    </div>
-  </div>
-  <div class="card__image site-card__image">
-    <img src="/assets/placeholder/image-1.jpg" />
-  </div>
-  <div class="card__footer">
-    My first Card Footer
-  </div>
-</article>
 
 {% for card in cardTypes %}
 
