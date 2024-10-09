2fbb4a974e175389148b68f4d3f18091c20da918
Merritt Informatics <daniel.merritt@informatics-studio.com>
2024-10-09 10:13:11 -0400

    scroll slider and card shortcode

--- docs-src/src/templates/shortcodes/index.js
+++ docs-src/src/templates/shortcodes/index.js
@@ -1,9 +1,11 @@
 import popoverGridItem from "./popoverGridItem.js";
 import tileGridItem from "./tileGridItem.js";
 import scrollSlider from "./scrollSlider.js";
+import card from "./card.js";
 
 export const shortcodes = {
   popoverGridItem,
   tileGridItem,
-  scrollSlider
+  scrollSlider,
+  card
 };
--- docs-src/src/templates/shortcodes/scrollSlider.js
+++ docs-src/src/templates/shortcodes/scrollSlider.js
@@ -1,42 +1,39 @@
+import card from "./card.js";
+
 export default function(
-  track = "test title", 
-  controls = "Information"
+  cards = 3,
+  title = 'Scroll Slider'
 ) {
+  let cardContent = ``;
+  for(let i = cards; i > 0; i--) {
+    cardContent = cardContent + `
+        <li class="scroll-slider__slide " data-ulu-scroll-slider-slide>
+          ${ card(cards - i + 1) } 
+        </li>
+      `;
+  }
   return `
-    <section class="scroll-slider-section container-full" aria-labelledby="slider-title670540522040c">
+    <section class="scroll-slider-section container-full" aria-labelledby="slider-title">
+      <div class="scroll-slider-section__title container-fit">
+        <h2 class="h2 h2--no-rule no-margin-bottom" id="slider-title">
+          ${ title }
+        </h2>
+      </div>
       <div class="scroll-slider-section__slides">
-        <div class="scroll-slider scroll-slider--cards" data-ulu-scroll-slider=''>
-        <div data-ulu-scroll-slider-control-context=''>
-          <div class="scroll-slider__track-crop" data-ulu-scroll-slider-track-container="">
-            <ul class="scroll-slider__track" data-ulu-scroll-slider-track=''>
-              <li class="scroll-slider__slide scroll-slider__slide--empty" role="presentation" data-ulu-scroll-slider-slide="">
-                &nbsp;
-              </li>
-              <li class="scroll-slider__slide " data-ulu-scroll-slider-slide="">
-                <div class="card">
-                  <div class="card__body">
-                    <a class="card__title" href="/knowledge-resources/toolkit/46933/eviction-prevention-toolkit">
-                      <span class="tag-container">
-                        <span class="tag site-content-type">New Toolkit</span>
-                      </span>
-                      <strong>Eviction Prevention: A Toolkit for Tenants and Service Providers</strong>
-                    </a>
-                    <div class="wysiwgy">
-                      <p>
-                        <span>This Toolkit outlines strategies for supporting people with mental health conditions, substance use disorders, or co-occurring disorders living in community-based low-income housing to prevent eviction.</span></p>
-                      <p>
-                        <a class="button" href="/knowledge-resources/toolkit/46933/eviction-prevention-toolkit">
-                          Download the Toolkit
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li class="scroll-slider__slide ">Item 2</li>
-            </ul>
+        <div class="scroll-slider scroll-slider--cards" data-ulu-scroll-slider="[]">
+          <div class="scroll-slider__control-context" data-ulu-scroll-slider-control-context>
+            <div class="scroll-slider__track-crop" data-ulu-scroll-slider-track-container>
+              <ul class="scroll-slider__track" data-ulu-scroll-slider-track>
+                <li class="scroll-slider__slide scroll-slider__slide--empty" role="presentation" data-ulu-scroll-slider-slide>
+                  &nbsp;
+                </li>
+                ${ cardContent }
+                <li class="scroll-slider__slide scroll-slider__slide--empty" role="presentation" data-ulu-scroll-slider-slide>
+                  &nbsp;
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </section>
