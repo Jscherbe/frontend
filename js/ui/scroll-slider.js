338cffffe6388ea8eb85a116c44a8011a39c9d29
Merritt Informatics <daniel.merritt@informatics-studio.com>
2024-10-11 13:58:16 -0400

    Remove scroll-slider extending OverflowScroller

--- js/ui/scroll-slider.js
+++ js/ui/scroll-slider.js
@@ -25,20 +25,7 @@ const queryAllInitial = key => document.querySelectorAll(attrSelectorInitial(key
 
 
 const instances = [];
-// Add in icon and labels
-// @daniel won't need to extend
-class SiteScrollSlider extends OverflowScroller {
-  constructor(...args) {
-    super(...args);
-  }
-  // update this in the OverflowScroller module
-  getControlContent(action) {
-    return `
-      <span class="hidden-visually">${ action === 'next' ? 'Scroll Left' : 'Scroll Right' }</span>
-      <span class="Slider__control-icon fas fa-chevron-${ action === 'next' ? 'right' : 'left' }" aria-hidden="true"></span>
-    `;
-  }
-}
+
 const defaults = {
   amount: createPager()
 };
@@ -52,7 +39,6 @@ export function init() {
   setup();
 }
 
-// document.querySelectorAll("[data-ulu-scroll-slider]").forEach(init);
 export function setup() {
   const builders = document.querySelectorAll(attrSelectorInitial("slider"));
   builders.forEach(setupSlider);
@@ -67,6 +53,5 @@ function setupSlider(container) {
     track: container.querySelector(attrSelector("track")),
     controls: container.querySelector(attrSelector("controls"))
   };
-  // replace with OverflowScroller when finished removing sitescrollslider
-  instances.push(new SiteScrollSlider(elements, config));
+  instances.push(new OverflowScroller(elements, config));
 }
