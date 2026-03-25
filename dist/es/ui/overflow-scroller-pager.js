2a2f17b83e91363196be9649dd270ae03b20795a
Joe Scherben <joe.scherben@informatics-studio.com>
2026-03-25 11:01:17 -0400

    Deploy updates

--- dist/es/ui/overflow-scroller-pager.js
+++ dist/es/ui/overflow-scroller-pager.js
@@ -1,26 +1,26 @@
 function y() {
-  return function(c, a) {
-    const r = a === "next", { track: s } = c.elements;
+  return function(d, c) {
+    const a = c === "next", { track: s } = d.elements;
     if (!s.children) return 400;
-    const g = window.getComputedStyle(s).getPropertyValue("scroll-padding-left").replace("auto", "0px"), n = parseInt(g, 10), { scrollLeft: i, offsetWidth: l } = s, u = i + l, o = [...s.children].map((e) => {
-      const { offsetLeft: t, offsetWidth: d } = e;
+    const g = window.getComputedStyle(s).getPropertyValue("scroll-padding-left").replace("auto", "0px"), o = parseInt(g, 10) || 0, { scrollLeft: n, offsetWidth: i } = s, u = n + i, f = [...s.children].map((e) => {
+      const { offsetLeft: t, offsetWidth: l } = e;
       return {
         element: e,
         offsetLeft: t,
-        offsetRight: t + d
+        offsetRight: t + l
       };
     });
-    let f;
-    if (r)
-      f = o.find((e) => e.offsetRight >= u);
+    let r;
+    if (a)
+      r = f.find((e) => e.offsetRight > u - 1);
     else {
-      let e = o.findLastIndex((t) => t.offsetLeft <= i);
-      if (e) {
-        let t = o[e];
-        f = o.slice(0, e + 1).find((h) => h.offsetLeft + n + l >= t.offsetRight);
+      let e = f.findLastIndex((t) => t.offsetLeft < n + o - 1);
+      if (e > -1) {
+        let t = f[e];
+        r = f.slice(0, e + 1).find((h) => h.offsetLeft - o + i >= t.offsetRight - 1) || t;
       }
     }
-    return f ? r ? f.offsetLeft : f.offsetLeft + n : 400;
+    return r ? Math.max(0, r.offsetLeft - o) : 400;
   };
 }
 export {
